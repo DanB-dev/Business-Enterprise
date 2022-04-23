f1ca7899c4aba76478ba531658ebef7b3447909e
z0diack <jakevardy@gmail.com>
2022-04-23 20:11:49 +0100

    Test work
    createTest will now simply generate a test and redirect you to the list
    of tests created, I have put in a timestamp so that the instructor can
    find the one that they have created. Implemented a UUID so that the ID
    is automatically created for each test.
    Test/id will now take you to the test and load the questions.
    STILL NEED TO HANDLE ANSWERING THE QUESTIONS.

--- src/main/java/uk/ac/bangor/csee/group3/spring/academigymraeg/model/Test.java
+++ src/main/java/uk/ac/bangor/csee/group3/spring/academigymraeg/model/Test.java
@@ -1,5 +1,7 @@
 package uk.ac.bangor.csee.group3.spring.academigymraeg.model;
 
+
+import java.time.Instant;
 import java.util.List;
 
 import javax.persistence.ElementCollection;
@@ -8,15 +10,34 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+
 @Entity
+
 public class Test {
 
+	@CreatedDate
+	private Instant createdDate;
+
+	public Instant getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Instant createdDate) {
+		this.createdDate = createdDate;
+	}
+
 	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
 	private String id;
 
 	@Embedded
 	@ElementCollection
 	private List<Question> questions;
+
+
 
 	public String getId() {
 		return id;
@@ -25,6 +46,8 @@ public class Test {
 	public List<Question> getQuestions() {
 		return questions;
 	}
+
+
 
 	public void setId(String id) {
 		this.id = id;
--- src/main/java/uk/ac/bangor/csee/group3/spring/academigymraeg/web/TestManagementController.java
+++ src/main/java/uk/ac/bangor/csee/group3/spring/academigymraeg/web/TestManagementController.java
@@ -1,5 +1,6 @@
 package uk.ac.bangor.csee.group3.spring.academigymraeg.web;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -8,9 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import uk.ac.bangor.csee.group3.spring.academigymraeg.RepositoryBasedNounImpl;
 import uk.ac.bangor.csee.group3.spring.academigymraeg.RepositoryBasedTestImpl;
@@ -36,20 +36,29 @@ public class TestManagementController {
 		List<Question> generatedTest = generateQuestions(2);
 		
 		model.addAttribute("generatedTest",generatedTest);
-		
-		return "createTest";
-	}
-	
-	@PostMapping("/createTest")
-	public String saveTest(@ModelAttribute List<Question> questions, Model model) {
 		Test toSave = new Test();
-		toSave.setQuestions(questions);
-		toSave.setId("3");
+		toSave.setQuestions(generatedTest);
+		toSave.setCreatedDate(Instant.now());
 		repository.save(toSave);
-		return "redirect:/welcome";
+		return "redirect:/tests";
 	}
 	
+	@RequestMapping("/tests")
+	public String showTests(Model model) {
+		List<Test> loadedTests = testDetails.loadAllTests();
+		model.addAttribute("loadedTests", loadedTests);
+		
+		return "tests";
+	}
 	
+	@GetMapping("/test/{id}")
+	public String showTest(@PathVariable("id") String id, Model model) {
+		Test toShow = testDetails.loadTestById(id);
+		List<Question> questions = toShow.getQuestions();
+		model.addAttribute("toShow",toShow);
+		model.addAttribute("questions",questions);
+		return "test";
+	}
 	
 	
 	private List<Question> generateQuestions(int number) {
